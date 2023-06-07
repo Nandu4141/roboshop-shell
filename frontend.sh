611ccca112fced1c2ef50d522010602051e090b4
peram rajasekhar reddy <133085616+Nandu4141@users.noreply.github.com>
2023-06-07 17:03:51 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,8 @@ yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-#we need copy conf file /etc/nginx/default.d/roboshop.conf
+cp roboshop.conf  /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
