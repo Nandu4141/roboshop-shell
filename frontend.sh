03d0f2decc168853646f954df3981125b987e076
CA Rajasekhar reddy <anurudh223@gmail.com>
2023-06-07 16:42:27 +0530

    frontend scrift

--- frontend.sh
+++ frontend.sh
@@ -4,5 +4,5 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-#we need copy conf file /etc/nginx/default.d/roboshop.conf
+cp roboshop.conf  /etc/nginx/default.d/roboshop.conf
 systemctl restart nginx
