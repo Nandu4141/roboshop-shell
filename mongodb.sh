c772c0af11ad9f6faed5a918b1c15954c4d1b1d0
CA Rajasekhar reddy <anurudh223@gmail.com>
2023-06-09 09:14:47 +0530

    mongodb scrift

--- mongodb.sh
+++ mongodb.sh
@@ -2,5 +2,5 @@ cp mongodb.repo  /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-#we change local host here
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
