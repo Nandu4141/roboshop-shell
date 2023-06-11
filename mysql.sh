726381c70fd0a4972718ea2daaf68add45ad0483
CA Rajasekhar reddy <anurudh223@gmail.com>
2023-06-11 09:55:19 +0530

    mysql  scrift

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,10 @@
 yum module disable mysql -y
+
 cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+
+
