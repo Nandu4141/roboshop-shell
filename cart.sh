80dfd8becd7317d80a09ea6c6a696ddfac1b3f06
CA Rajasekhar reddy <anurudh223@gmail.com>
2023-06-11 09:22:30 +0530

    cart scrift

--- cart.sh
+++ cart.sh
@@ -3,11 +3,12 @@ yum install nodejs -y
 useradd roboshop
 rm -rf /app
 mkdir /app
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
-unzip /tmp/user.zip
+unzip /tmp/cart.zip
 cd /app
 npm install
+
 cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
 systemctl daemon-reload
